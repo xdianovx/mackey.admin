0b2ca82e8ae58f1ffdc6dc07c9979c003afcf754
John Doe <johndoe@example.com>
2024-06-01 01:39:53 +0300

    UPD

--- pages/products/all.vue
+++ pages/products/all.vue
@@ -14,11 +14,7 @@ const searchRef = ref();
 </script>
 
 <template>
-  <pre>
 
-    {{ prods }}
-    </pre
-  >
   <div class="flex items-center">
     <UiTitle tag="h1">Список товаров</UiTitle>
 
@@ -31,27 +27,11 @@ const searchRef = ref();
     <FormSearchBar v-model="searchRef" />
 
     <ProductCategories class="mt-6" :data="categories" />
-    <!-- <div class="mt-6">
-      <UiTitle tag="h2">Применённые фильтры</UiTitle>
-      <div class="flex gap-2 mt-3"></div>
 
-      <div
-        class="bg-[#F4F1FD] inline-block border font-medium border-[#C6B6F7] leading-[20px] text-accent py-[4px] px-3 rounded-lg"
-      >
-        Для неё
-      </div>
-    </div> -->
   </section>
 
   <section class="mt-8">
-    <!-- <div class="flex px-5 text-[12px] leading-[24px] text-[#6C6C89]">
-      <div class="w-[76px]">Все</div>
-      <div class="w-[188px] text-center">Название</div>
-      <div class="w-[86px] text-center">Цвет</div>
-      <div class="w-[86px] text-center">Артикул</div>
-      <div class="w-[86px] text-center">Наличие</div>
-      <div class="w-[86px] text-center">Цена</div>
-    </div> -->
+
     <div class="mt-4 bg-[#F7F7F8] flex flex-col gap-2 rounded-[24px] p-[6px]">
       <ProductsProductItem
         v-for="item in prods"
